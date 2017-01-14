504f3b5225098cc0f93e2c1b462eec96d86d65fb
Daniel Lemper <ich@dlemper.de>
2017-01-14 21:53:25 +0100

    promisified and es2015

--- lcd.js
+++ lcd.js
@@ -1,14 +1,15 @@
 'use strict';
 
-var EventEmitter = require('events').EventEmitter,
-  rpio = require('rpio'),
-  Q = require('q'),
-  util = require('util'),
-  tick = global.setImmediate || process.nextTick;
+const rpio = require('rpio');
+const Q = require('q');
+const util = require('util');
+const tick = global.setImmediate || process.nextTick;
 
-var __ROW_OFFSETS = [0x00, 0x40, 0x14, 0x54];
+const delay = (time) => new Promise(fulfill => setTimeout(fulfill, time));
 
-var __COMMANDS = {
+const __ROW_OFFSETS = [0x00, 0x40, 0x14, 0x54];
+
+const __COMMANDS = {
   CLEAR_DISPLAY: 0x01,
   HOME: 0x02,
   SET_CURSOR: 0x80,
@@ -27,13 +28,9 @@ var __COMMANDS = {
 };
 
 function Lcd(config) {
-  var i;
-
   if (!(this instanceof Lcd)) {
     return new Lcd(config);
   }
-
-  EventEmitter.call(this);
 
   this.cols = config.cols || 16; // TODO - Never used, remove?
   this.rows = config.rows || 1;
@@ -51,7 +48,7 @@ function Lcd(config) {
   };
 
   this.data = []; // data bus, db4 thru db7, outputs, initially low
-  for (i = 0; i < config.data.length; i += 1) {
+  for (let i = 0; i < config.data.length; i += 1) {
     const pin = config.data[i];
     rpio.open(pin, rpio.OUTPUT, rpio.LOW);
     this.data.push({
@@ -68,7 +65,6 @@ function Lcd(config) {
   this.init();
 }
 
-util.inherits(Lcd, EventEmitter);
 module.exports = Lcd;
 
 // private
@@ -96,71 +92,30 @@ Lcd.prototype.init = () => {
   .then(() => this._command(this.displayControl))
   .then(() => this._command(this.displayMode))
   .then(() => this._command(0x01)) // clear display (don't call clear to avoid event)
-  .delay(3)             // wait > 1.52ms for display to clear
-  .then(() => this.emit('ready'));
+  .delay(3);             // wait > 1.52ms for display to clear
 };
 
 Lcd.prototype.print = (val, cb) => {
-  this._queueAsyncOperation((cb2) => {
-    var index,
-      displayFills;
+  val += '';
 
-    val += '';
+  // If n*80+m characters should be printed, where n>1, m<80, don't display the
+  // first (n-1)*80 characters as they will be overwritten. For example, if
+  // asked to print 802 characters, don't display the first 720.
+  const displayFills = Math.floor(val.length / 80);
+  const index = displayFills > 1 ? (displayFills - 1) * 80 : 0;
 
-    // If n*80+m characters should be printed, where n>1, m<80, don't display the
-    // first (n-1)*80 characters as they will be overwritten. For example, if
-    // asked to print 802 characters, don't display the first 720.
-    displayFills = Math.floor(val.length / 80);
-    index = displayFills > 1 ? (displayFills - 1) * 80 : 0;
-
-    this._printChar(val, index, cb, cb2);
-  }.bind(this));
+  return this._printChar(val, index);
 };
 
 // private
-Lcd.prototype._printChar = (str, index, cb, cb2) => {
-  tick(() => {
-    if (index >= str.length) {
-      if (cb) {
-        cb(null, str);
-      } else {
-        this.emit('printed', str);
-      }
+Lcd.prototype._printChar = (str, index) => this._write(str.charCodeAt(index)).then(() => this._printChar(str, index + 1));
 
-      return cb2(null);
-    }
+Lcd.prototype.clear = () => this._commandAndDelay(__COMMANDS.CLEAR_DISPLAY, 3);
 
-    try {
-      this._write(str.charCodeAt(index));
-      this._printChar(str, index + 1, cb, cb2);
-    } catch (e) {
-      if (cb) {
-        cb(e);
-      } else {
-        this.emit('error', e);
-      }
-
-      return cb2(e);
-    }
-  }.bind(this));
-};
-
-Lcd.prototype.clear = (cb) => {
-  this._queueAsyncOperation((cb2) => {
-    // Wait > 1.52ms. There were issues waiting for 2ms so wait 3ms.
-    this._commandAndDelay(__COMMANDS.CLEAR_DISPLAY, 3, 'clear', cb, cb2);
-  }.bind(this));
-};
-
-Lcd.prototype.home = (cb) => {
-  this._queueAsyncOperation((cb2) => {
-    // Wait > 1.52ms. There were issues waiting for 2ms so wait 3ms.
-    this._commandAndDelay(__COMMANDS.HOME, 3, 'home', cb, cb2);
-  }.bind(this));
-};
+Lcd.prototype.home = () => this._commandAndDelay(__COMMANDS.HOME, 3);
 
 Lcd.prototype.setCursor = (col, row) => {
-  var r = row > this.rows ? this.rows - 1 : row; //TODO: throw error instead? Seems like this could cause a silent bug.
+  const r = row > this.rows ? this.rows - 1 : row; //TODO: throw error instead? Seems like this could cause a silent bug.
   //we don't check for column because scrolling is a possibility. Should we check if it's in range if scrolling is off?
   return this._command(__COMMANDS.SET_CURSOR | (col + __ROW_OFFSETS[r]));
 };
@@ -220,58 +175,16 @@ Lcd.prototype.noAutoscroll = () => {
 };
 
 Lcd.prototype.close = () => {
-  var i;
-
   this.rs.unexport();
   this.e.unexport();
 
-  for (i = 0; i < this.data.length; i += 1) {
+  for (let i = 0; i < this.data.length; i += 1) {
     this.data[i].unexport();
   }
 };
 
 // private
-Lcd.prototype._queueAsyncOperation = (asyncOperation) => {
-  this.asyncOps.push(asyncOperation);
-
-  if (this.asyncOps.length === 1) {
-    (function next() {
-      this.asyncOps[0](() => {
-        this.asyncOps.shift();
-        if (this.asyncOps.length !== 0) {
-          next.bind(this)();
-        }
-      }.bind(this));
-    }.bind(this)());
-  }
-}
-
-// private
-Lcd.prototype._commandAndDelay = (command, timeout, event, cb, cb2) => {
-  tick(() => {
-    try {
-      this._command(command);
-    } catch (e) {
-      if (cb) {
-        cb(e);
-      } else {
-        this.emit('error', e);
-      }
-
-      return cb2(e);
-    }
-
-    setTimeout(() => {
-      if (cb) {
-        cb(null);
-      } else {
-        this.emit(event);
-      }
-
-      return cb2(null);
-    }.bind(this), timeout);
-  }.bind(this));
-};
+Lcd.prototype._commandAndDelay = (command, timeout) => this._command(command).then(() => Q.delay(timeout));
 
 // private
 Lcd.prototype._command = (cmd) => this._send(cmd, 0);
@@ -288,18 +201,19 @@ Lcd.prototype._send = (val, mode) => {
 
 // private
 Lcd.prototype._write4Bits = (val) => {
-  if(!(typeof val === 'number')){
+  if (!(typeof val === 'number')) {
     throw new Error("Value passed to ._write4Bits must be a number");
   }
 
-  var i;
+  const dataProm = [];
 
-  for (i = 0; i < this.data.length; i += 1, val = val >> 1) {
-    this.data[i].writeSync(val & 1);
+  for (let i = 0; i < this.data.length; i += 1, val = val >> 1) {
+    dataProm.push(this.data[i].writeSync(val & 1));
   }
 
   // enable pulse >= 300ns
-  return this.e.writeSync(1)
-  .then(() => Q.delay(30))
+  return Promise.all(dataProm)
+  .then(() => this.e.writeSync(1))
+  .then(() => delay(30))
   .then(() => this.e.writeSync(0));
 };
