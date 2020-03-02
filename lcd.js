d3a249b98e4fca79617cf8700093a0d2825eee9c
Daniel Lemper <ich@dlemper.de>
2020-03-03 00:05:55 +0100

    fix to heavy promise usage, which leads to better memory usage

--- lcd.js
+++ lcd.js
@@ -1,11 +1,7 @@
-'use strict';
-
 const rpio = require('rpio');
-const util = require('util');
-const tick = global.setImmediate || process.nextTick;
+const { promisify } = require('util');
 
-const delay = (time, ...args) => new Promise(resolve => setTimeout(resolve, time, ...args));
-const sequentialPromises = funcs => funcs.reduce((promise, func) => promise.then(result => func().then(Array.prototype.concat.bind(result))), Promise.resolve([]));
+const wait = promisify(setTimeout);
 
 const ROW_OFFSETS = [0x00, 0x40, 0x10, 0x50]; // TODO: make this configurable, as the original values ([0x00, 0x40, 0x14, 0x54]) weren't correct
 
@@ -33,7 +29,7 @@ class Lcd {
     this.displayMode = 0x06; // left to right, no shift
   }
 
-  init(config) {
+  async init(config) {
     this.rows = config.rows || 1;
     this.largeFont = !!config.largeFont;
 
@@ -41,78 +37,91 @@ class Lcd {
     this.e = this._configurePin(config.e);
     this.data = config.data.map(pin => this._configurePin(pin));
 
-    return delay(16)                    // wait > 15ms
-      .then(() => this._write4Bits(0x03)) // 1st wake up
-      .then(() => delay(6))               // wait > 4.1ms
-      .then(() => this._write4Bits(0x03)) // 2nd wake up
-      .then(() => delay(2))               // wait > 160us
-      .then(() => this._write4Bits(0x03)) // 3rd wake up
-      .then(() => delay(2))               // wait > 160us
-      .then(() => this._write4Bits(0x02)) // 4 bit interface
-      .then(() => this._send(0x20 | ((this.rows > 1) ? 0x08 : 0x00) | ((this.rows === 1 && this.largeFont) ? 0x24 : 0x00)))
-      .then(() => this._send(0x10))
-      .then(() => this._send(this.displayControl))
-      .then(() => this._send(this.displayMode))
-      .then(() => this._send(0x01)) // clear display (don't call clear to avoid event)
-      .then(() => delay(3))            // wait > 1.52ms for display to clear
-      .catch(console.error);
+    await wait(16);              // wait > 15ms
+    await this._write4Bits(0x03); // 1st wake up
+    await wait(6);               // wait > 4.1ms
+    await this._write4Bits(0x03); // 2nd wake up
+    await wait(2);               // wait > 160us
+    await this._write4Bits(0x03); // 3rd wake up
+    await wait(2);               // wait > 160us
+    await this._write4Bits(0x02); // 4 bit interface
+    await this._send(0x20
+      | ((this.rows > 1) ? 0x08 : 0x00)
+      | ((this.rows === 1 && this.largeFont) ? 0x24 : 0x00));
+    await this._send(0x10);
+    await this._send(this.displayControl);
+    await this._send(this.displayMode);
+    await this.clear();             
   }
 
-  print(val) {
+  async print(val) {
     val += '';
 
     // If n*80+m characters should be printed, where n>1, m<80, don't display the
     // first (n-1)*80 characters as they will be overwritten. For example, if
     // asked to print 802 characters, don't display the first 720.
     const displayFills = Math.floor(val.length / 80);
-    const index = displayFills > 1 ? (displayFills - 1) * 80 : 0;
+    const index = displayFills > 1
+      ? (displayFills - 1) * 80
+      : 0;
 
-    return sequentialPromises(val.split('').map(n => () => this._send(n.charCodeAt(0), 1)));
+    for (c of val.split('')) {
+      await this._send(c.charCodeAt(0), 1);
+    }
   }
 
-  clear() {
-    return this._send(COMMANDS.CLEAR_DISPLAY)
-      .then(() => delay(3));
+  async clear() {
+    await this._send(COMMANDS.CLEAR_DISPLAY);
+    await wait(3); // wait > 1.52ms for display to clear
   }
 
-  home() {
-    return this._send(COMMANDS.HOME)
-      .then(() => delay(3));
+  async home() {
+    await this._send(COMMANDS.HOME);
+    await wait(3);
   }
 
   setCursor(col, row) {
-    const r = row > this.rows ? this.rows - 1 : row; //TODO: throw error instead? Seems like this could cause a silent bug.
+    const r = row > this.rows
+      ? this.rows - 1
+      : row; //TODO: throw error instead? Seems like this could cause a silent bug.
+    
     //we don't check for column because scrolling is a possibility. Should we check if it's in range if scrolling is off?
     return this._send(COMMANDS.SET_CURSOR | (col + ROW_OFFSETS[r]));
   }
 
   display() {
     this.displayControl |= COMMANDS.DISPLAY_ON;
+
     return this._send(this.displayControl);
   }
 
   noDisplay() {
     this.displayControl &= COMMANDS.DISPLAY_OFF;
+
     return this._send(this.displayControl);
   }
 
   cursor() {
     this.displayControl |= COMMANDS.CURSOR_ON;
+
     return this._send(this.displayControl);
   }
 
   noCursor() {
     this.displayControl &= COMMANDS.CURSOR_OFF;
+
     return this._send(this.displayControl);
   }
 
   blink() {
     this.displayControl |= COMMANDS.BLINK_ON;
+
     return this._send(this.displayControl);
   }
 
   noBlink() {
     this.displayControl &= COMMANDS.BLINK_OFF;
+
     return this._send(this.displayControl);
   }
 
@@ -126,21 +135,25 @@ class Lcd {
 
   leftToRight() {
     this.displayMode |= COMMANDS.LEFT_TO_RIGHT;
+
     return this._send(this.displayMode);
   }
 
   rightToLeft() {
     this.displayMode &= COMMANDS.RIGHT_TO_LEFT;
+
     return this._send(this.displayMode);
   }
 
   autoscroll() {
     this.displayMode |= COMMANDS.AUTOSCROLL_ON;
+
     return this._send(this.displayMode);
   }
 
   noAutoscroll() {
     this.displayMode &= COMMANDS.AUTOSCROLL_OFF;
+
     return this._send(this.displayMode);
   }
 
@@ -152,29 +165,28 @@ class Lcd {
 
   _configurePin(pin) {
     rpio.open(pin, rpio.OUTPUT, rpio.LOW); // reg. select, output, initially low
+
     return {
-      writeSync: value => Promise.resolve(rpio.write(pin, value)),
+      writeSync: value => rpio.write(pin, value),
       unexport: () => rpio.close(pin),
     };
   }
 
-  _send(val, mode = 0) {
-    return this.rs.writeSync(mode)
-      .then(() => this._write4Bits(val >> 4))
-      .then(() => this._write4Bits(val & 15))
-      .catch(console.error);
+  async _send(val, mode = 0) {
+    this.rs.writeSync(mode);
+    await this._write4Bits(val >> 4);
+    await this._write4Bits(val & 15);
   }
 
-  _write4Bits(val) {
+  async _write4Bits(val) {
     if (!(typeof val === 'number')) {
       throw new Error("Value passed to ._write4Bits must be a number");
     }
 
-    return Promise.all(this.data.map((n, idx) => n.writeSync((val >> idx) & 1)))
-      .then(() => this.e.writeSync(1))
-      .then(() => delay(1)) // enable pulse >= 300ns
-      .then(() => this.e.writeSync(0))
-      .catch(console.error);
+    this.data.forEach((n, idx) => n.writeSync((val >> idx) & 1));
+    this.e.writeSync(1);
+    await wait(1); // enable pulse >= 300ns
+    this.e.writeSync(0);
   }
 }
 
